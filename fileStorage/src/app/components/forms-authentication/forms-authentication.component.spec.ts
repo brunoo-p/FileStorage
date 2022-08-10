8a4865d49d40a795390b2f8f993538bb121c7ee0
Bruno <brun.op@hotmail.com>
2022-08-10 14:36:54 -0300

    fix tests

--- fileStorage/src/app/components/forms-authentication/forms-authentication.component.spec.ts
+++ fileStorage/src/app/components/forms-authentication/forms-authentication.component.spec.ts
@@ -1,3 +1,5 @@
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FormsAuthenticationComponent } from './forms-authentication.component';
@@ -8,6 +10,7 @@ describe('FormsAuthenticationComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ RouterTestingModule, HttpClientTestingModule ],
       declarations: [ FormsAuthenticationComponent ]
     })
     .compileComponents();
